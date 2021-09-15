67aad26d26f3bb48661caed77e9d2a5dff008413
Brent Cameron <wwbd28@gmail.com>
2021-09-15 19:01:54 -0400

    Made directory, selected variables, cleaned data

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -4,52 +4,62 @@
 #this script loads the raw data, processes and cleans it 
 #and saves it as Rds file in the processed_data folder
 
+#The link for the data set used can be found here:
+#https://data.cdc.gov/Case-Surveillance/COVID-19-Case-Surveillance-Public-Use-Data-with-Ge/n8mc-b4w4
+
 #load needed packages. make sure they are installed.
 library(readxl) #for loading Excel files
 library(dplyr) #for data processing
 library(here) #to set paths
+library(utils) #to read csv file
 
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
-
+data_location <- here::here("MADA","BrentCameron-MADA-analysis2","data","raw_data","COVID-19_Case_Surveillance_Public_Use_Data_with_Geography.csv")
+                            
 #load data. 
-#note that for functions that come from specific packages (instead of base R)
-# I often specify both package and function like so
-#package::function() that's not required one could just call the function
-#specifying the package makes it clearer where the function "lives",
-#but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+rawdata <- utils::read.csv(data_location)
 
 #take a look at the data
 dplyr::glimpse(rawdata)
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+#Examine the data more closely to ensure everything is working properly
+View(rawdata)
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#The outcome of interest will be COVID-19 deaths
+#Variables that will serve as predictors of interest will include...
+#Ethnicity,age group, and whether or not there is an underlying condition
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
+#Additionally, some variables (such as case month, and residents state) will 
+#be included to assess whether there are noticeable differences in the data
+#based on time and geography, particularly between states with varying amounts
+#of minority racial groups, and to analyze the role of ethnicity in
+#a resilience(or susceptibility) to COVID-19, possibly due to a good support
+#system (or lack thereof) or other factors
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
+processeddata <- rawdata %>% 
+  select("death_yn", "ethnicity", "age_group", "underlying_conditions_yn",
+         "case_month", "res_state")
 
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
+#This is a large file with over 2 million observations, naturally, there are 
+#many missing observations (N/A), in particular for recorded death and ethnicity
+#To handle this, we will utilize pairwise deletion to omit all observations that
+#have a missing value in either recorded death, or ethnicity depending on what 
+#data testing we are doing at the time
+#we can alternate if needed to gather more reliable data
+
+#Pairwise deletion for missing death variable (death_yn)
+processeddata[! is.na (processeddata$death_yn),]
+
+#Pairwise deletion for missing ethnicity variables (ethnicity)
+processeddata[! is.na (processeddata$ethnicity)]
+  
+#Make sure the processed data looks right
+
+dplyr::glimpse(processeddata)
+
+#One last check
+View(processeddata)
 
 # save data as RDS
 # I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
@@ -59,8 +69,7 @@ processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>%
 # http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
 
 # location to save file
-save_data_location <- here::here("data","processed_data","processeddata.rds")
+save_data_location <- here::here("MADA","BrentCameron-MADA-analysis2","data",
+"raw_data","COVID-19_Case_Surveillance_Public_Use_Data_with_Geography.csv")
 
 saveRDS(processeddata, file = save_data_location)
-
-
